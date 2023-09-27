bb321c3b9a674a068f0794c98dcaf1915aa40e92
yangwenfeng <yangwenfeng.0719@bytedance.com>
2023-09-27 15:14:06 +0800

    feat: modify playground style

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,5 +1,6 @@
 import Cropper from "cropperjs";
 
+import "cropperjs/dist/cropper.css";
 import "./style.css";
 
 const $demoImage = document.querySelector<HTMLImageElement>("#demo-image");
@@ -7,14 +8,6 @@ const $demoImage = document.querySelector<HTMLImageElement>("#demo-image");
 if ($demoImage !== null) {
   const cropper = new Cropper($demoImage, {
     aspectRatio: 16 / 9,
-    crop(event) {
-      console.log(event.detail.x);
-      console.log(event.detail.y);
-      console.log(event.detail.width);
-      console.log(event.detail.height);
-      console.log(event.detail.rotate);
-      console.log(event.detail.scaleX);
-      console.log(event.detail.scaleY);
-    },
+    preview: ".img-preview",
   });
 }
